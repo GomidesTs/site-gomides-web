8f82930951410d00c64d4bc86d760545d33bab7e
GomidesTs <gomidestulio5@gmail.com>
2023-07-30 21:11:46 -0300

    style: color

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -6,7 +6,7 @@ type ButtonProps = {
 
 export const Button = ({ text }: ButtonProps) => {
   return (
-    <div className="mt-4 flex h-12 w-2/4 cursor-pointer items-center justify-center rounded bg-blue-300 text-base font-medium text-white-300 transition delay-200 hover:bg-blue-500">
+    <div className="mt-4 flex h-12 w-2/4 cursor-pointer items-center justify-center rounded bg-blue-300 text-base font-medium text-white-300 transition delay-200 ease-out hover:bg-blue-400">
       <Link href="https://wa.me/5531997390128" target="_blank">
         {text}
       </Link>
