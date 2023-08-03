6b0c31ca326636b94205ac8bdc8c41aba8c2f917
GomidesTs <gomidestulio5@gmail.com>
2023-08-03 13:56:43 -0300

    feat: privacy policy
    Added to the rules of the privacy policy.

--- src/app/privacy/page.tsx
+++ src/app/privacy/page.tsx
@@ -1,7 +1,7 @@
 export default function Privacy() {
   return (
     <main className="mt-16 flex flex-col gap-3">
-      <h1>Politica de privacidade</h1>
+      <h1 className="mb-4 text-3xl">Politica de privacidade</h1>
 
       <p>
         Esta política de privacidade descreve como Gomidesweb faz o tratamento
@@ -11,6 +11,327 @@ export default function Privacy() {
         informa como coletamos e tratamos estes dados quando você acessa nosso
         Site e descreve os direitos do usuário de acessar, corrigir ou
         restringir o uso de seus dados pessoais.
+      </p>
+
+      <h3 className="text-xl font-bold uppercase">Leitura importante</h3>
+
+      <p>
+        Para acessar ao nosso Site e nossos serviços, o Usuário declara que leu,
+        compreende e concorda com a nosso Aviso de Privacidade e autoriza o
+        tratamento de seus dados e informações pessoais para as finalidades que
+        indicamos.
+      </p>
+
+      <h3 className="text-xl font-bold uppercase">Quais dados coletamos</h3>
+
+      <p>
+        Podemos coletar as informações que o Usuário insere quando preenche
+        formulários no Site e informações que são coletadas automaticamente
+        quando o Usuário acessa nosso Site.
+      </p>
+
+      <h3 className="text-xl font-bold uppercase">
+        Informações inseridas pelo Usuário
+      </h3>
+
+      <p>
+        <span className="font-medium uppercase">Cadastro em Newsletter:</span>{' '}
+        Para que você possa receber nossos e-mails com nossas atualizações e
+        novidades, solicitamos que você os informe seu nome e e-mail. A
+        finalidade do tratamento desses dados é apenas para que fins de
+        marketing e comunicação. Fazemos o tratamento desses dados com base no
+        seu consentimento (art. 7, I, da LGPD).
+      </p>
+
+      <p>
+        <span className="font-medium uppercase">
+          Cadastro para participação de eventos gratuitos:
+        </span>{' '}
+        Para que você possa se inscrever e ter acesso à nossas aulas gratuitas,
+        solicitamos que você nos informe seu nome e e-mail. A finalidade do
+        tratamento desses dados é apenas para que fins de sua inscrição e
+        compartilhar as principais informações, notícias importantes, materiais
+        do evento e o link dos nossos encontros. Fazemos o tratamento desses
+        dados com base no seu consentimento (art. 7, I, da LGPD).
+      </p>
+
+      <p>
+        <span className="font-medium uppercase">
+          Grupos em aplicativos de mensagens:
+        </span>{' '}
+        Quando você participa de nossos grupos nos aplicativos de mensagens
+        temos acesso ao nome que você cadastrou no seu aplicativo e ao seu
+        número de telefone. A finalidade do tratamento desses dados é apenas
+        para que fins de marketing e comunicação sobre nossos cursos. Fazemos o
+        tratamento desses dados com base no seu consentimento (art. 7, I, da
+        LGPD).
+      </p>
+
+      <p>
+        <span className="font-medium uppercase">Torpedo de voz:</span> Quando
+        você se cadastra seus dados para participar dos nossos eventos gratuitos
+        podemos enviar mensagens de voz para convidar você para assistir as
+        aulas. A finalidade do tratamento desses dados é apenas para que fins de
+        marketing e comunicação sobre nossos cursos. Fazemos o tratamento desses
+        dados com base no seu consentimento (art. 7, I, da LGPD).
+      </p>
+
+      <p>
+        <span className="font-medium uppercase">Torpedo de voz:</span> Quando
+        você se cadastra seus dados para participar dos nossos eventos gratuitos
+        podemos enviar mensagens de voz para convidar você para assistir as
+        aulas. A finalidade do tratamento desses dados é apenas para que fins de
+        marketing e comunicação sobre nossos cursos. Fazemos o tratamento desses
+        dados com base no seu consentimento (art. 7, I, da LGPD).
+      </p>
+
+      <p>
+        <span className="font-medium uppercase">Contato:</span> Dados pessoais
+        que você fornece quando reporta um problema no nosso website ou quando
+        necessita de suporte ao cliente, quando entra em contato por telefone,
+        e-mail ou de outra forma.
+      </p>
+
+      <h3 className="text-xl font-bold uppercase">
+        Informações coletadas automaticamente
+      </h3>
+
+      <p>
+        Podemos coletar algumas informações de modo automático, como: URLs, IPs
+        de acesso, páginas acessadas, pesquisas efetuadas e outras informações
+        sobre sua navegação. Essas informações são coletadas através de cookies,
+        que são arquivos utilizados para melhorar a experiência de navegação do
+        Usuário no Site de acordo com suas preferências. Para mais informações
+        consulte nosso Aviso de Cookies. A coleta de informações de IP é
+        obrigatória por determinação do Marco Civil da Internet, Lei
+        12.965/2014.
+      </p>
+
+      <h3 className="text-xl font-bold uppercase">
+        Compartilhamento de dados pessoais
+      </h3>
+
+      <p>
+        Não compartilhamos seus dados pessoais com nenhuma outra empresa, os
+        dados coletados apenas serão acessados por colaboradores da Gomidesweb.
+        e direcionados para os setores responsáveis para atender sua
+        solicitação. Para obter informações sobre o compartilhamento dos seus
+        dados pessoais entre em contato através do e-mail gomidesweb@gmail.com.
+      </p>
+
+      <h3 className="text-xl font-bold uppercase">
+        Transferência internacional de dados
+      </h3>
+
+      <p>
+        Agência Gomidesweb possui sua sede no Brasil e destina seus serviços
+        para pessoas localizadas no Brasil, aplicando-se, portanto, à lei
+        brasileira de proteção de dados pessoais. Contudo, os dados pessoais dos
+        Usuários poderão ser armazenados fora do território nacional, através do
+        serviço de computação na nuvem. Essas transferências são realizadas
+        apenas para empresas que estejam em conformidade com legislações de
+        proteção de dados e que mantenham um nível de proteção semelhante ou
+        mais rigoroso do que o previsto na legislação brasileira. Para obter
+        informações sobre a transferência internacional dos seus dados pessoais
+        entre em contato através do e-mail gomidesweb@gmail.com.
+      </p>
+
+      <h3 className="text-xl font-bold uppercase">
+        Tempo de armazenamento dos dados
+      </h3>
+
+      <p>
+        Armazenaremos os dados pessoais do Usuário pelo tempo que for necessário
+        para cumprir com as finalidades para as quais os coletamos, inclusive
+        para fins de cumprimento de quaisquer obrigações legais, contratuais, de
+        prestação de contas ou requisição de autoridades competentes. Todos os
+        dados coletados serão excluídos de nossos servidores quando o Usuário
+        requisitar ou quando estes não forem mais necessários ou relevantes para
+        oferecermos os nossos serviços, salvo se houver qualquer outra razão
+        para a sua manutenção, como eventual obrigação legal de retenção de
+        dados.
+      </p>
+
+      <h3 className="text-xl font-bold uppercase">
+        Quais são os seus direitos
+      </h3>
+
+      <p>
+        Atendendo à Lei Geral de Proteção de Dados Pessoais, Lei 13.709/2018
+        (&ldquo;LGPD&rdquo;), a qualquer momento o Usuário pode solicitar:
+      </p>
+
+      <ol className="m-auto flex w-[90%] list-decimal flex-col gap-3 xl:w-full">
+        <li>A confirmação da existência de tratamento;</li>
+        <li>O acesso aos dados;</li>
+        <li>A correção de dados incompletos, inexatos ou desatualizados;</li>
+        <li>
+          A anonimização, bloqueio ou eliminação de dados desnecessários,
+          excessivos ou tratados em desconformidade;
+        </li>
+        <li>
+          A portabilidade de seus dados a outro fornecedor de serviço ou
+          produto, mediante requisição expressa pelo Usuário;
+        </li>
+        <li>A eliminação dos dados tratados com consentimento do Usuário;</li>
+        <li>
+          A obtenção de informações sobre as entidades públicas ou privadas com
+          as quais os seus dados foram compartilhados;
+        </li>
+        <li>
+          A informação sobre a possibilidade de não fornecer o seu
+          consentimento, bem como de ser informado sobre as consequências, em
+          caso de negativa;
+        </li>
+        <li>A revogação do consentimento.</li>
+      </ol>
+
+      <h3 className="text-xl font-bold uppercase">
+        Como exercer seus direitos
+      </h3>
+
+      <p>
+        Caso o Usuário necessite de qualquer auxílio para exercer seus direitos,
+        poderá entrar em contato, enviando um e mail para o e-mail
+        gomidesweb@gmail.com. As solicitações serão atendidas dentro de um prazo
+        de 15 (quinze) dias. O Usuário pode solicitar a revogação do seu
+        consentimento, mas algumas funcionalidades do Site e alguns serviços
+        oferecidos pela Empresa poderão ficar, total ou parcialmente,
+        indisponíveis.
+      </p>
+
+      <p>
+        O Usuário pode solicitar a exclusão de seus dados, mas só poderemos
+        fazer a exclusão após um período mínimo de seis meses. Este é o prazo
+        mínimo para guarda de informações de Usuários de aplicações de internet,
+        determinado pelo art. 15, do Marco Civil da Internet, Lei 12.965/2014.
+      </p>
+
+      <h3 className="text-xl font-bold uppercase">
+        Como protegemos suas informações de dados pessoais
+      </h3>
+
+      <p>
+        Adotamos medidas técnicas de segurança e organizacionais para proteger
+        suas informações e dados pessoais. Os dados serão armazenados em um
+        ambiente operacional seguro que não é acessível ao público. Entretanto,
+        nenhuma transmissão pela Internet pode ser garantida como completamente
+        segura. Consequentemente, não garantimos que informações pessoais
+        estarão protegidas contra invasão de &ldquo;hackers&rdquo; ou qualquer
+        outra ação ilegal cometida por terceiros no ambiente da internet. Por
+        isso, é importante que você também tenha um comportamento seguro,
+        identificando e evitando situações que possam ameaçar a segurança dos
+        seus dados. Também é muito importante que o Usuário saiba que não
+        enviamos mensagens eletrônicas solicitando confirmação de dados ou com
+        anexos que possam ser executados (extensões: .exe, .com, entre outros)
+        ou ainda links para eventuais downloads. Caso você identifique ou tome
+        conhecimento de algo que comprometa a segurança dos dados pessoais,
+        pedimos que entre em contato conosco através do e-mail
+        gomidesweb@gmail.com.
+      </p>
+
+      <h3 className="text-xl font-bold uppercase">
+        Link para sites, plataformas e portais
+      </h3>
+
+      <p>
+        Ao clicar em links existentes no Site o Usuário pode ser direcionado
+        para outros sites, portais ou plataformas de outras empresas. Estas
+        empresas têm políticas de privacidade e tratamento de dados próprias e
+        devem ser observadas pelo Usuário. Cabe ao Usuário ler a Política de
+        Privacidade de sites, portais ou plataformas de outras empresas, sendo
+        de responsabilidade do Usuário aceitá-la ou rejeitá-la.
+      </p>
+
+      <h3 className="text-xl font-bold uppercase">Grupos de WhatsApp</h3>
+
+      <p>
+        Após sua inscrição em nossos eventos você poderá participar do nosso
+        grupo exclusivo do WhatsApp para ter acesso às principais informações,
+        notícias importantes, materiais do evento e links dos nossos encontros.
+      </p>
+
+      <p>Para manter a boa convivência em grupo temos algumas regras:</p>
+
+      <ol className="m-auto flex w-[90%] list-disc flex-col gap-3 xl:w-full">
+        <li>
+          Esse será o único grupo oficial e exclusivo para envio de informações;
+        </li>
+        <li>
+          Ao ingressar e permanecer no grupo você concorda com o tratamento de
+          seus dados pessoais (telefone e nome que você cadastrou no seu
+          aplicativo), para fins estatísticos, marketing e comunicação;
+        </li>
+        <li>Você pode sair do grupo a qualquer momento;</li>
+        <li>
+          O tratamento dos dados será realizado enquanto perdurar você
+          permanecer no grupo ou até que você solicite a exclusão ou eliminação
+          dos seus dados. Para mais informações entre em contato como nosso DPO
+          através do e-mail gomidesweb@gmail.com;
+        </li>
+        <li>
+          Não nos responsabilizamos pela criação de grupos externos. Ao fazer
+          parte do grupo os demais participantes poderão visualizar seu nome e
+          seu número de telefone. Fique atento e para evitar golpes tenha muito
+          cuidado ao abrir mensagens e arquivos recebidos de números
+          desconhecidos numa conversa privada;
+        </li>
+        <li>
+          É terminantemente proibido aos participantes utilizar os dados
+          pessoais dos demais participantes do grupo para qualquer finalidade
+          comercial ou de marketing. Isto configura infração à Lei Geral de
+          Proteção de Dados e sujeita o infrator à denúncias e medidas judiciais
+          ou extrajudiciais cabíveis;
+        </li>
+        <li>
+          Em caso de abuso ou recebimento de arquivos e mensagens indevidas ou
+          suspeitas, sem sua autorização, pedimos que entre em contato direto
+          como nosso DPO, através do e-mail gomidesweb@gmail.com, para que
+          possamos tomar as medidas cabíveis;
+        </li>
+        <li>
+          O envio de mensagens desrespeitosas por parte do participante,
+          contendo palavras de baixo calão, insultos ou material impróprio,
+          nocivo ou ilegal acarretará a imediata exclusão do grupo, sem aviso
+          prévio, e o bloqueio do número de telefone utilizado dentro do
+          WhatsApp para futuras interações, bem como poderá ocasionar sanções
+          administrativas, cíveis e penais acaso cabíveis, caso em que a
+          mensagem será mantida para os devidos fins de prova;
+        </li>
+        <li>
+          {' '}
+          Os dados pessoais tornados manifestamente públicos por você em
+          comentários realizados nos grupos de WhatsApp não se enquadram como
+          dados a serem protegidos, não necessitando de consentimento o
+          tratamento destes dados (art. 7°, §4°, da LGPD), estando a empresa
+          isenta de qualquer responsabilidade por danos decorrentes de sua livre
+          exposição (art. 43, III, da LGPD);
+        </li>
+        <li>
+          O envio e o recebimento das mensagens e comunicações estão
+          condicionados à questões técnicas, tais como a disponibilidade de
+          sinal de Internet e a disponibilidade do próprio aplicativo WhatsApp;
+        </li>
+      </ol>
+
+      <h3 className="text-xl font-bold uppercase">
+        Atualizações deste Aviso de Privacidade
+      </h3>
+
+      <p>
+        Buscamos sempre aprimorar nossos padrões de segurança, por isso nosso
+        Aviso de Privacidade poderá ser atualizada com frequência. Assim,
+        recomendamos que o Usuário acesse nossa Política periodicamente, para
+        que tenha conhecimento sobre eventuais modificações.
+      </p>
+
+      <h3 className="text-xl font-bold uppercase">
+        Como entrar em contato conosco
+      </h3>
+
+      <p>
+        Se você tiver dúvidas ou desejar obter mais informações sobre nossa
+        Política de Privacidade, entre em contato conosco através do e-mail
       </p>
     </main>
   );
