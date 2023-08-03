849656102e9e077a0d07d7fbd39fd970eb58f344
GomidesTs <gomidestulio5@gmail.com>
2023-08-02 22:13:55 -0300

    docs: orthography
    Fixed the spelling of the last term on the usage policies page.

---
+++ src/app/privacy/page.tsx
@@ -0,0 +1,17 @@
+export default function Privacy() {
+  return (
+    <main className="mt-16 flex flex-col gap-3">
+      <h1>Politica de privacidade</h1>
+
+      <p>
+        Esta política de privacidade descreve como Gomidesweb faz o tratamento
+        de dados pessoais quando você usa este website. Este Aviso de
+        Privacidade demonstra nosso compromisso com a segurança, a privacidade e
+        a transparência no tratamento de suas informações e dados pessoais,
+        informa como coletamos e tratamos estes dados quando você acessa nosso
+        Site e descreve os direitos do usuário de acessar, corrigir ou
+        restringir o uso de seus dados pessoais.
+      </p>
+    </main>
+  );
+}
