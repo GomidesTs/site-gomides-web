cffa3b32c183a0ee7fe5d0f5f4b6f916c1e71c6b
GomidesTs <gomidestulio5@gmail.com>
2023-08-02 21:01:23 -0300

    feat: terms
    Criado a página de termos de uso com os termos de usabilidade.

---
+++ src/app/terms/page.tsx
@@ -0,0 +1,262 @@
+export default function Terms() {
+  return (
+    <main className="mt-16 flex flex-col gap-3">
+      <h1 className="mb-4 text-3xl">Termos de uso</h1>
+
+      <p>
+        Este documento, e todo o conteúdo do site é oferecido por Gomidesweb de
+        cnpj 48.4444.561/0001-06, neste termo representado apenas por &ldquo;
+        <span className="uppercase">empresa</span>&rdquo;, que regulamenta todos
+        os direitos e obrigações com todos que acessam o site, denominado neste
+        termo como &ldquo;<span className="uppercase">visitante</span>&ldquo;,
+        reguardado todos os direitos previstos na legislação, trazem as
+        cláusulas abaixo como requisito para acesso e visita do mesmo, situado
+        no endereço gomidesweb.com.br e seus subdomínios.
+      </p>
+
+      <p>
+        A permanência no website implica-se automaticamente na leitura e
+        aceitação tácita do presente termos de uso a seguir. Este termo foi
+        atualizado pela última vez em 02 de agosto de 2023.
+      </p>
+
+      <h3 className="text-xl font-bold uppercase">1. Da função do site</h3>
+
+      <p>
+        Este site foi criado e desenvolvido com a função de trazer conteúdo
+        informativo de alta qualidade, a venda de produtos físicos, digitais e a
+        divulgação de prestação de serviço. A{' '}
+        <span className="uppercase">empresa</span> busca através da criação de
+        conteúdo de alta qualidade, desenvolvido por profissionais da área,
+        trazer o conhecimento ao alcance de todos, assim como a divulgação dos
+        próprios serviços.
+      </p>
+
+      <p>
+        Nesta plataforma, poderá ser realizado tanto a divulgação de material
+        original de alta qualidade, assim como a divulgação de produtos de
+        e-commerce.
+      </p>
+
+      <p>
+        Todo o conteúdo presente neste site foi desenvolvido buscando fontes e
+        materiais de confiabilidade, assim como são baseados em estudos sérios e
+        respeitados, através de pesquisa de alta nível.
+      </p>
+
+      <p>
+        Todo o conteúdo é atualizado periodicamente, porém, pode conter em algum
+        artigo, vídeo ou imagem, alguma informação que não reflita a verdade
+        atual, não podendo a <span className="uppercase">empresa</span> ser
+        responsabilizada de nenhuma forma ou meio por qualquer conteúdo que não
+        esteja devidamente atualizado.
+      </p>
+
+      <h3 className="text-xl font-bold uppercase">2. Do Glossário</h3>
+
+      <p>
+        Este termo pode conter algumas palavras específicas que podem não se de
+        conhecimento geral. Entre elas:
+      </p>
+
+      <p>
+        <span className="font-medium uppercase">Visitante: </span> Todo e
+        qualquer usuário do site, de qualquer forma e meio, que acesse através
+        de computador, notebook, tablet, celular ou quaisquer outros meios, o
+        website ou plataforma da empresa.
+      </p>
+
+      <p>
+        <span className="font-medium uppercase">Navegação: </span> O ato de
+        visitar páginas e conteúdo do website ou plataforma da empresa. COOKIES:
+        Pequenos arquivos de textos gerados automaticamente pelo site e
+        transmitido para o navegador do visitante, que servem para melhorar a
+        usabilidade do visitante.
+      </p>
+
+      <p>
+        <span className="font-medium uppercase">Hiperlinks: </span> São links
+        clicáveis que podem aparecer pelo site ou no conteúdo, que levam para
+        outra página da <span className="uppercase">empresa</span> ou site
+        externo.
+      </p>
+
+      <p>
+        Em caso de dúvidas sobre qualquer palavra utilizada neste termo, o
+        <span className="uppercase">visitante</span> deverá entrar em contato
+        com a <span className="uppercase">empresa</span> através dos canais de
+        comunicação encontradas no site.
+      </p>
+
+      <h3 className="text-xl font-bold uppercase">4. Do acesso ao site</h3>
+
+      <p>
+        O Site e plataforma funcionam normalmente 24 (vinte e quatro) horas por
+        dia, porém podem ocorrer pequenas interrupções de forma temporária para
+        ajustes, manutenção, mudança de servidores, falhas técnicas ou por ordem
+        de força maior, que podem deixar o site indisponível por tempo limitado.
+      </p>
+
+      <p>
+        A <span className="uppercase">empresa</span> não se responsabiliza por
+        nenhuma perda de oportunidade ou prejuízos que esta indisponibilidade
+        temporária possa gerar aos usuários.
+      </p>
+
+      <p>
+        Em caso de manutenção que exigirem um tempo maior, a{' '}
+        <span className="uppercase">empresa</span> irá informar previamente aos
+        clientes da necessidade e do tempo previsto em que o site ou plataforma
+        ficará offline.
+      </p>
+
+      <p>
+        Caso seja necessário realizar um cadastro junto a plataforma, onde o
+        <span className="uppercase">visitante</span> deverá preencher um
+        formulário com seus dados e informações, para ter acesso a alguma parte
+        restrita, ou realizar alguma compra.
+      </p>
+
+      <p>
+        Todos os dados estão protegidos conforme a Lei Geral de Proteção de
+        Dados, e ao realizar o cadastro junto ao site, o{' '}
+        <span className="uppercase">visitante</span> concorda integralmente com
+        a coleta de dados conforme a Lei e com a Política de Privacidade da{' '}
+        <span className="uppercase">empresa</span>.
+      </p>
+
+      <h3 className="text-xl font-bold uppercase">
+        5. Da licença de uso e cópia
+      </h3>
+
+      <p>
+        O visitante poderá acessar todo o conteúdo do website, como artigos,
+        vídeos, imagens, produtos e serviços, não significando nenhum tipo de
+        cessão de direito ou permissão de uso, ou de cópia dos mesmo.
+      </p>
+
+      <p>
+        Todos os direitos são preservados, conforme a legislação brasileira,
+        principalmente na Lei de Direitos Autorais (regulamentada na Lei nº
+        9.610/18), assim como no Código Civil brasileiro (regulamentada na Lei
+        nº 10.406/02), ou quaisquer outras legislações aplicáveis.
+      </p>
+
+      <p>
+        Todo o conteúdo do site é protegido por direitos autorais, e seu uso,
+        cópia, transmissão, venda, cessão ou revenda, deve seguir a lei
+        brasileira, tendo a <span className="uppercase">empresa</span> todos os
+        seus direitos reservados, e não permitindo a cópia ou utilização de
+        nenhuma forma e meio, sem autorização expressa e por escrita da mesma.
+      </p>
+
+      <p>
+        A <span className="uppercase">empresa</span> poderá em casos concretos
+        permitir pontualmente exceções a este direito, que serão claramente
+        destacados no mesmo, com a forma e permissão de uso do conteúdo
+        protegido. Este direito é revogável e limitado as especificações de cada
+        caso.
+      </p>
+
+      <h3 className="text-xl font-bold uppercase">6. Das obrigações</h3>
+
+      <p>
+        O <span className="uppercase">visitante</span> ao utilizar o website da{' '}
+        <span className="uppercase">empresa</span>, concorda integralmente em:
+      </p>
+
+      <p>
+        De nenhuma forma ou meio realizar qualquer tipo de ação que tente
+        invadir, hacker, destruir ou prejudicar a estrutura do site, plataforma
+        da <span className="uppercase">empresa</span> ou de seus parceiros
+        comerciais. Incluindo-se, mas não se limitando, ao envio de vírus de
+        computador, de ataques de DDOS, de acesso indevido por falhas da mesma
+        ou quaisquer outras forma e meio.
+      </p>
+
+      <p>
+        De não realizar divulgação indevida nos comentários do site de conteúdo
+        de SPAM, empresas concorrentes, vírus, conteúdo que não possua direitos
+        autorais ou quaisquer outros que não seja pertinente a discussão daquele
+        texto, vídeo ou imagem.
+      </p>
+
+      <p>
+        Da proibição em reproduzir qualquer conteúdo do site ou plataforma sem
+        autorização expressa, podendo responder civil e criminalmente pelo
+        mesmo.
+      </p>
+
+      <p>
+        Com a Política de Privacidade do site, assim como tratamos os dados
+        referentes ao cadastro e visita no site, podendo a qualquer momento e
+        forma, requerer a exclusão dos mesmos, através do formulário de contato.
+      </p>
+
+      <h3 className="text-xl font-bold uppercase">
+        7. Da motetização e publicidade
+      </h3>
+
+      <p>
+        A <span className="uppercase">empresa</span> pode alugar ou vender
+        espaços publicitários na plataforma, ou no site, diretamente aos
+        anunciantes, ou através de empresas especializadas com o Adsense
+        (Google), Taboola ou outras plataformas especializadas como o Eletro
+        Criticas
+      </p>
+
+      <p>
+        Essas publicidades não significam nenhuma forma de endosso ou
+        responsabilidade pelos mesmos, ficando o{' '}
+        <span className="uppercase">visitante</span> responsável pelas compras,
+        visitas, acessos ou quaisquer ações referentes as estas empresas.
+      </p>
+
+      <p>
+        Todas as propagandas no site ou plataforma serão claramente destacadas
+        como publicidade, como forma de disclaimer da{' '}
+        <span className="uppercase">empresa</span> e de conhecimento do
+        <span className="uppercase">visitante</span>.
+      </p>
+
+      <p>
+        Em casos de compra de produtos ou serviços, será possível a devolução em
+        até 07 (sete) dias, conforme o Código de Defesa do Consumidor.
+      </p>
+
+      <p>
+        Estes anúncios podem ser selecionados pela empresa de publicidade
+        automaticamente, conforme as visitas recentes do{' '}
+        <span className="uppercase">visitante</span>, assim como baseado no seu
+        histórico de busca, conforme as políticas de acesso da plataforma.
+      </p>
+
+      <h3 className="text-xl font-bold uppercase">8. Dos termos gerais</h3>
+
+      <p>
+        O Site irá apresentar hiperlinks durante toda a sua navegação, que podem
+        levar diretamente para outra página da{' '}
+        <span className="uppercase">empresa</span> ou para sites externos.
+      </p>
+
+      <p>
+        Apesar da <span className="uppercase">empresa</span> apenas criar links
+        para sites externos de extrema confiança, caso o usuário acesse um site
+        externo, a <span className="uppercase">empresa</span> não tem nenhuma
+        responsabilidade pelo meio, sendo uma mera indicação de complementação
+        de conteúdo, ficando o mesmo responsável pelo acesso, assim como sobre
+        quaisquer ações que venham a realizar neste site.
+      </p>
+
+      <p>
+        Em caso que ocorra eventuais conflitos judiciais entre o{' '}
+        <span className="uppercase">visitante</span> e a
+        <span className="uppercase">empresa</span>, o foro elegido para a devida
+        ação será o da comarca da Empresa, mesmo que haja outro mais
+        privilegiado.
+      </p>
+
+      <p>Ponte Nova, MG, 02 de Agosto de 2023</p>
+    </main>
+  );
+}
