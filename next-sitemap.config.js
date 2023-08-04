189a524de2c62fa6234568312317dd86c0d2094a
GomidesTs <gomidestulio5@gmail.com>
2023-08-03 22:25:14 -0300

    refactor: sitemap
    Changed system settings to enter as much information as possible.

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,35 @@
+/** @type {import('next-sitemap').IConfig} */
+
 module.exports = {
   siteUrl: 'https://www.gomidesweb.com.br/',
+  changefreq: 'weekly',
+  priority: 0.7,
+  sitemapSize: 5000,
   generateRobotsTxt: true,
-  sitemapSize: 7000
+  author: ['Tulio Gomides', 'Tiago Gomides'],
+
+  transform: async (config, path) => {
+    return {
+      loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      alternateRefs: config.alternateRefs ?? []
+    };
+  },
+  additionalPaths: async (config) => [
+    await config.transform(config, '/additional-page')
+  ],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/'
+      },
+      {
+        userAgent: 'test-bot',
+        allow: ['/', '/cookies', '/privacy', '/redirect', '/terms']
+      }
+    ]
+  }
 };
