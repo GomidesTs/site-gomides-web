61f37a3459352bcf255ceab925b8137db341f802
GomidesTs <gomidestulio5@gmail.com>
2023-08-10 10:54:11 -0300

    docs: text and style
    Changing placeholder texts and styling

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Logo } from '../Logo';
 import { useDiscord } from './useDiscord';
 
 export const Form = () => {
@@ -15,38 +16,40 @@ export const Form = () => {
   return (
     <form
       onSubmit={handleSubmit(handleFormSubmit)}
-      className="m-auto flex w-full max-w-xl flex-col gap-2 rounded bg-white-100 p-4"
+      className="m-auto flex w-full max-w-xl flex-col gap-2"
     >
-      <div className="flex flex-col gap-1">
-        <label htmlFor="name">Nome</label>
-        <input
-          id="name"
-          type="text"
-          {...register('fields.name')}
-          placeholder="Escreva seu nome"
-          className="rounded p-2"
-        />
-        {errors.fields?.name?.message && (
-          <p className="text-red-500 transition-all duration-500">
-            {errors.fields?.name?.message}
-          </p>
-        )}
-      </div>
+      <div className="flex flex-col gap-2 lg:flex-row">
+        <div className="flex w-full flex-col gap-1">
+          <label htmlFor="name">Nome</label>
+          <input
+            id="name"
+            type="text"
+            {...register('fields.name')}
+            placeholder="Digite seu nome"
+            className="rounded border border-t-gray-500/80 bg-white-300 p-2 focus:border-blue-300 focus:outline-none focus:ring-1"
+          />
+          {errors.fields?.name?.message && (
+            <p className="text-red-500 transition-all duration-500">
+              {errors.fields?.name?.message}
+            </p>
+          )}
+        </div>
 
-      <div className="flex flex-col gap-1">
-        <label htmlFor="company">Nome da Empresa</label>
-        <input
-          id="company"
-          type="text"
-          {...register('fields.company')}
-          placeholder="Nome da Empresa"
-          className="rounded p-2"
-        />
-        {errors.fields?.company?.message && (
-          <p className="text-red-500 transition-all duration-500">
-            {errors.fields?.company?.message}
-          </p>
-        )}
+        <div className="flex w-full flex-col gap-1">
+          <label htmlFor="company">Nome da Empresa</label>
+          <input
+            id="company"
+            type="text"
+            {...register('fields.company')}
+            placeholder="Digite o nome da sua empresa"
+            className="rounded border border-t-gray-500/80 bg-white-300 p-2 focus:border-blue-300 focus:outline-none focus:ring-1"
+          />
+          {errors.fields?.company?.message && (
+            <p className="text-red-500 transition-all duration-500">
+              {errors.fields?.company?.message}
+            </p>
+          )}
+        </div>
       </div>
 
       <div className="flex flex-col gap-1">
@@ -55,8 +58,8 @@ export const Form = () => {
           id="email"
           type="text"
           {...register('fields.email')}
-          placeholder="email@example.com"
-          className="rounded p-2"
+          placeholder="Digite seu melhor e-mail"
+          className="rounded border border-t-gray-500/80 bg-white-300 p-2 focus:border-blue-300 focus:outline-none focus:ring-1"
         />
         {errors.fields?.email?.message && (
           <p className="text-red-500 transition-all duration-500">
@@ -72,7 +75,7 @@ export const Form = () => {
           type="tel"
           {...register('fields.phone')}
           placeholder="(99) 9 9999-9999"
-          className="rounded p-2"
+          className="rounded border border-t-gray-500/80 bg-white-300 p-2 focus:border-blue-300 focus:outline-none focus:ring-1"
         />
         {errors.fields?.phone?.message && (
           <p className="text-red-500 transition-all duration-500">
@@ -85,54 +88,58 @@ export const Form = () => {
         <h6>Como podemos te ajudar?</h6>
         <div className="flex gap-1">
           {optionsServices.map((service) => (
-            <div
-              key={service.value}
-              className="flex w-full items-center justify-between"
-            >
-              <div className="flex flex-grow-0 items-center gap-1">
+            <div key={service.value} className="w-full">
+              <div className="flex flex-grow-0 items-center">
                 <input
                   id={service.value}
                   type="checkbox"
                   {...register('fields.services')}
                   value={service.value}
-                  className="rounded p-2"
+                  className="peer/draft hidden rounded border border-gray-500/80 bg-white-300 p-2 focus:border-blue-300 focus:outline-none focus:ring-1"
                 />
-                <label htmlFor={service.value}>{service.label}</label>
+                <label
+                  htmlFor={service.value}
+                  className="flex w-full items-center justify-center rounded border border-t-gray-500/80 p-2 hover:bg-blue-300/50 peer-checked/draft:border-blue-300/90 peer-checked/draft:bg-blue-300/90 peer-checked/draft:text-white-300"
+                >
+                  {service.label}
+                </label>
               </div>
             </div>
           ))}
-          {errors.fields?.services?.message && (
-            <p className="text-red-500 transition-all duration-500">
-              {errors.fields?.services?.message}
-            </p>
-          )}
         </div>
+        {errors.fields?.services?.message && (
+          <p className="text-red-500 transition-all duration-500">
+            {errors.fields?.services?.message}
+          </p>
+        )}
       </div>
 
       <div className="flex flex-col gap-1">
-        <h6>Quantos colaboradores fazem parte da sua empresa?</h6>
-        <div className="flex gap-1">
+        <h6>Qual o prazo que temos para entregar o projeto?</h6>
+        <div className="flex gap-1 ">
           {optionsTerm.map((term) => (
-            <div
-              key={term.value}
-              className="flex-grow-1 flex items-center gap-1"
-            >
+            <div key={term.value} className="flex items-center gap-1 text-sm">
               <input
                 id={term.value}
                 type="radio"
                 {...register('fields.term')}
                 value={term.value}
-                className="rounded p-2"
+                className="peer/draft hidden rounded border border-t-gray-500/80 bg-white-300 p-2 focus:border-blue-300 focus:outline-none focus:ring-1"
               />
-              <label htmlFor={term.value}>{term.label}</label>
+              <label
+                htmlFor={term.value}
+                className="rounded border border-t-gray-500/80 p-2 hover:bg-blue-300/50 peer-checked/draft:border-blue-300/90 peer-checked/draft:bg-blue-300/90 peer-checked/draft:text-white-300"
+              >
+                {term.label}
+              </label>
             </div>
           ))}
-          {errors.fields?.term?.message && (
-            <p className="text-red-500 transition-all duration-500">
-              {errors.fields?.term?.message}
-            </p>
-          )}
         </div>
+        {errors.fields?.term?.message && (
+          <p className="text-red-500 transition-all duration-500">
+            {errors.fields?.term?.message}
+          </p>
+        )}
       </div>
 
       <div className="flex flex-col gap-1">
@@ -140,8 +147,8 @@ export const Form = () => {
         <textarea
           id="description"
           {...register('fields.description')}
-          placeholder="Faca uma breve descricao"
-          className="rounded p-2"
+          placeholder="Compartilhe o que você considera fundamental sobre o projeto."
+          className="h-56 resize-none rounded border border-t-gray-500/80 bg-white-300 p-2 focus:border-blue-300 focus:outline-none focus:ring-1"
         />
         {errors.fields?.description?.message && (
           <p className="text-red-500 transition-all duration-500">
@@ -150,9 +157,24 @@ export const Form = () => {
         )}
       </div>
 
-      <button type="submit" className="mt-4 rounded bg-blue-300 p-4">
-        Enviar
-      </button>
+      <div className="flex justify-between">
+        <button
+          type="submit"
+          className="mt-4 rounded bg-blue-300 p-4 text-white-300"
+        >
+          Enviar
+        </button>
+
+        <div className="flex items-center gap-2">
+          <Logo />
+          <div>
+            <h6 className="text-sm font-bold">Prefere uma reunião?</h6>
+            <p className="cursor-pointer text-sm text-black-500">
+              Agende com a Gio
+            </p>
+          </div>
+        </div>
+      </div>
     </form>
   );
 };
