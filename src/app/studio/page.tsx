39028f8d4cb56d7b566cd520a2c88e6392b3fd89
GomidesTs <gomidestulio5@gmail.com>
2023-08-12 16:53:41 -0300

    feat: studio
    Creation of the page about and the studio

---
+++ src/app/studio/page.tsx
@@ -0,0 +1,241 @@
+export default function studio() {
+  return (
+    <main className="mb-8 mt-12 lg:mt-60">
+      <section>
+        <div className="flex items-center justify-start gap-6 opacity-70">
+          <div className="h-0.5 w-10 bg-gray-500" />
+          <p className="text-center text-sm font-normal uppercase tracking-wide text-gray-300">
+            Nós
+          </p>
+        </div>
+
+        <h1 className="mt-8 text-4xl font-bold text-blue-500 lg:mt-14 lg:text-8xl">
+          Seja Bem-Vindo à GomidesWeb
+        </h1>
+
+        <div className="mt-28 flex flex-col justify-between lg:flex-row">
+          <p>
+            Na GomidesWeb, revolucionamos sua marca com soluções digitais
+            poderosas. Nossas interfaces inovadoras aprimoram a jornada do
+            cliente, agregando valor em cada passo. Do design à codificação,
+            personalizamos cada etapa para transformar sua realidade online.
+            Somos um estúdio de design baseado em Belo Horizonte - MG,
+            especializados em criar websites incríveis. Nossas soluções são
+            visualmente deslumbrantes, intuitivas e geram resultados reais.
+            Acreditamos que experiências incríveis impulsionam visibilidade e
+            valor para as marcas. Eleve sua presença digital com a GomidesWeb.
+          </p>
+        </div>
+      </section>
+
+      <section className="mt-12 lg:mt-60">
+        <div className="flex items-center justify-start gap-6 opacity-70">
+          <div className="h-0.5 w-10 bg-gray-500" />
+          <p className="text-center text-sm font-normal uppercase tracking-wide text-gray-300">
+            Além da Tela
+          </p>
+        </div>
+
+        <h2 className="text-2xl text-blue-500 lg:mt-8 lg:text-5xl">
+          Descubra os criadores
+          <br />
+          que deram vida a este projeto.
+        </h2>
+
+        <div className="flex flex-col">
+          <div className="mt-8 flex gap-8">
+            <div className="h-52 w-52 bg-black-500"></div>
+
+            <div className="w-1/2">
+              <h3 className="text-lg font-semibold">Tiago Gomides</h3>
+
+              <p>
+                UI/UX e programação. Ele desempenha o papel de responsável
+                técnico pelo contato com o cliente, desde o início do projeto
+                até o acompanhamento pós-entrega.
+              </p>
+            </div>
+          </div>
+
+          <div className="mt-8 flex gap-8">
+            <div className="w-1/2">
+              <h3 className="text-lg font-semibold">Tulio Gomides</h3>
+
+              <p>
+                Abraçando a nobre tarefa de infundir vida ao design através do
+                domínio das linhas de código. Transformando conceitos visuais em
+                uma vibrante realidade digital, infundindo em cada projeto uma
+                essência distinta e pulsante.
+              </p>
+            </div>
+
+            <div className="h-52 w-52 bg-black-500"></div>
+          </div>
+        </div>
+
+        <div className="relative mt-28 h-px w-full bg-blue-500/80 before:absolute before:-top-1 before:right-0 before:h-1 before:w-8 before:bg-blue-500/80"></div>
+      </section>
+
+      <section className="mt-12">
+        <div className="flex items-center justify-start gap-6 opacity-70">
+          <div className="h-0.5 w-10 bg-gray-500" />
+          <p className="text-center text-sm font-normal uppercase tracking-wide text-gray-300">
+            Valores
+          </p>
+        </div>
+
+        <p className="mt-2 text-2xl">
+          Nosso DNA é forjado
+          <br />
+          pela paixão de criar e pelo desejo de causar impacto
+        </p>
+
+        <div className="mt-12 flex flex-col gap-4 text-white-300 lg:grid lg:grid-cols-3 lg:grid-rows-3">
+          <div className="flex h-80 w-full cursor-pointer flex-col justify-between rounded-2xl bg-black-500/50 p-4">
+            <span className="text-5xl font-bold">01</span>
+
+            <div>
+              <h3 className="text-3xl font-bold">Empatia</h3>
+
+              <p className="mt-2 text-sm">
+                Colocar-se no lugar dos outros para compreender suas
+                necessidades e perspectivas.
+              </p>
+            </div>
+          </div>
+
+          <div className="flex h-80 w-full cursor-pointer flex-col justify-between rounded-2xl bg-black-500/50 p-4">
+            <span className="text-5xl font-bold">02</span>
+
+            <div>
+              <h3 className="text-3xl font-bold">Integridade</h3>
+
+              <p className="mt-2 text-sm">
+                Priorizar a honestidade, ética e transparência em todas as
+                interações e decisões.
+              </p>
+            </div>
+          </div>
+
+          <div className="flex h-80 w-full cursor-pointer flex-col justify-between rounded-2xl bg-black-500/50 p-4">
+            <span className="text-5xl font-bold">03</span>
+
+            <div>
+              <h3 className="text-3xl font-bold">Inovação</h3>
+
+              <p className="mt-2 text-sm">
+                Fomentar uma cultura de criatividade e busca constante por novas
+                soluções e melhorias.
+              </p>
+            </div>
+          </div>
+
+          <div className="col-start-2 col-end-2 flex h-80 w-full cursor-pointer flex-col justify-between rounded-2xl bg-black-500/50 p-4">
+            <span className="text-5xl font-bold">04</span>
+
+            <div>
+              <h3 className="text-3xl font-bold">Colaboração</h3>
+
+              <p className="mt-2 text-sm">
+                Promover o trabalho em equipe, comunicação aberta e construção
+                de relacionamentos sólidos.
+              </p>
+            </div>
+          </div>
+
+          <div className="col-start-2 col-end-2 row-start-3 flex h-80 w-full cursor-pointer flex-col justify-between rounded-2xl bg-black-500/50 p-4">
+            <span className="text-5xl font-bold">05</span>
+
+            <div>
+              <h3 className="text-3xl font-bold">Excelência</h3>
+
+              <p className="mt-2 text-sm">
+                Buscar constantemente a qualidade superior e o desempenho
+                excepcional em todas as áreas.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="relative mt-28 h-px w-full bg-blue-500/80 before:absolute before:-top-1 before:left-0 before:h-1 before:w-8 before:bg-blue-500/80"></div>
+      </section>
+
+      <section className="mt-12">
+        <div className="flex items-center justify-start gap-6 opacity-70">
+          <div className="h-0.5 w-10 bg-gray-500" />
+          <p className="text-center text-sm font-normal uppercase tracking-wide text-gray-300">
+            Nossa jornada
+          </p>
+        </div>
+
+        <p className="mt-2 text-2xl">
+          Marcas inovadoras, moldando futuros excepcionais.
+        </p>
+
+        <div className="mb-12 mt-12 flex flex-col gap-4 lg:grid lg:grid-cols-3 lg:grid-rows-1">
+          <div className="flex items-center gap-4">
+            <div className="h-14 w-14 bg-black-500/80"></div>
+            <div>
+              <h5 className="font-bold">Empresa x</h5>
+
+              <p className="text-sm">
+                Criamos x completa para ajudar a x se posiocional como um x
+                profissional.
+              </p>
+            </div>
+          </div>
+
+          <div className="flex items-center gap-4">
+            <div className="h-14 w-14 bg-black-500/80"></div>
+            <div>
+              <h5 className="font-bold">Empresa x</h5>
+
+              <p className="text-sm">
+                Criamos x completa para ajudar a x se posiocional como um x
+                profissional.
+              </p>
+            </div>
+          </div>
+
+          <div className="flex items-center gap-4">
+            <div className="h-14 w-14 bg-black-500/80"></div>
+            <div>
+              <h5 className="font-bold">Empresa x</h5>
+
+              <p className="text-sm">
+                Criamos x completa para ajudar a x se posiocional como um x
+                profissional.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-28 flex gap-2">
+          <span className="-mt-14 text-8xl leading-none text-gray-300">
+            &#8220;
+          </span>
+
+          <div className="flex flex-col gap-3">
+            <p className="text-xl text-black-500/90 lg:text-4xl">
+              Desde o atendimento personalizado até o desenvolvimento do
+              projeto, só tenho elogios pelo excelente trabalho. O resultado
+              superou minhas expectativas e o projeto ficou ainda melhor do que
+              eu imaginava.
+            </p>
+
+            <p className="text-2xl font-medium text-black-500">
+              Daniela Martin
+            </p>
+
+            <p className="text-sm text-black-500/50">Eng. Alimentos</p>
+          </div>
+        </div>
+
+        <div className="flex justify-end">
+          <p className="font-bold">Explore nossos projetos</p>
+        </div>
+        <div className="relative mt-8 h-px w-full bg-blue-500/80 before:absolute before:-top-1 before:right-0 before:h-1 before:w-8 before:bg-blue-500/80"></div>
+      </section>
+    </main>
+  );
+}
