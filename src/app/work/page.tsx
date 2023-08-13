5a3663d22f949649d15cea48514fab5175abdbb8
GomidesTs <gomidestulio5@gmail.com>
2023-08-12 22:10:58 -0300

    feat: work
    Creation of the page to show the work done

---
+++ src/app/work/page.tsx
@@ -0,0 +1,206 @@
+export default function Work() {
+  return (
+    <main className="lg:my-28">
+      <section>
+        <div className="flex items-center justify-start gap-6 opacity-70">
+          <div className="h-0.5 w-10 bg-gray-500" />
+          <p className="text-center text-sm font-normal uppercase tracking-wide text-gray-300">
+            Explore
+          </p>
+        </div>
+
+        <h1 className="mt-8 text-4xl font-bold text-blue-500 lg:mt-14 lg:text-8xl">
+          Conheça nossa
+          <br />
+          expertise em ação
+        </h1>
+
+        <div className="flex justify-end">
+          <div className="mt-8 lg:mt-24 lg:w-2/4">
+            <div className="before:left-0-0 relative h-px w-32 bg-blue-500/50 before:absolute before:top-0 before:h-px before:w-8 before:bg-blue-500"></div>
+            <p className="mt-2 text-gray-300">
+              Ao longo dde nossa tragetoria, tivemos o privilégio de trabalhar
+              com empresas de diversos setores, ajudando-as a criar uma presença
+              online única e impactante. Confira abaixo alguns dos projetos que
+              realizamos e veja como podemos transformar a sua empresa também.
+            </p>
+          </div>
+        </div>
+
+        <div className="mt-12 flex flex-col gap-4 lg:grid lg:grid-cols-3 lg:grid-rows-5">
+          <div className="col-span-3 col-start-2 flex w-full flex-col gap-3">
+            <div>
+              <h2 className="text-xl font-bold uppercase tracking-widest">
+                Empresa x
+              </h2>
+
+              <p className="text-sm">
+                Criamos o novo website para esta empresa que vem crescendo muito
+                no mercado.
+              </p>
+
+              <span className="text-xs uppercase tracking-widest">Website</span>
+            </div>
+            <div className="group flex h-32 w-full cursor-pointer items-center justify-center bg-black-500/50 ease-out lg:h-56">
+              <p className="hidden p-8 text-2xl font-medium text-white-300 transition-all duration-500 group-hover:inline-flex">
+                ver projeto
+              </p>
+            </div>
+          </div>
+
+          <div className="col-span-1 row-start-2 flex w-full flex-col-reverse gap-3 lg:-mt-32 lg:flex-col">
+            <div className="group flex h-32 w-full cursor-pointer items-center justify-center bg-black-500/50 ease-out lg:h-56">
+              <p className="hidden p-8 text-2xl font-medium text-white-300 transition-all duration-500 group-hover:inline-flex">
+                ver projeto
+              </p>
+            </div>
+
+            <div>
+              <h2 className="text-xl font-bold uppercase tracking-widest">
+                Empresa x
+              </h2>
+
+              <p className="text-sm">
+                Criamos o novo website para esta empresa que vem crescendo muito
+                no mercado.
+              </p>
+
+              <span className="text-xs uppercase tracking-widest">Website</span>
+            </div>
+          </div>
+
+          <div className="col-span-3 row-start-3 flex w-full flex-col-reverse gap-3 lg:-mt-32 lg:flex-col">
+            <div className="group flex h-32 w-full cursor-pointer items-center justify-center bg-black-500/50 ease-out lg:h-56">
+              <p className="hidden p-8 text-2xl font-medium text-white-300 transition-all duration-500 group-hover:inline-flex">
+                ver projeto
+              </p>
+            </div>
+
+            <div>
+              <h2 className="text-xl font-bold uppercase tracking-widest">
+                Empresa x
+              </h2>
+
+              <p className="text-sm">
+                Criamos o novo website para esta empresa que vem crescendo muito
+                no mercado.
+              </p>
+
+              <span className="text-xs uppercase tracking-widest">Website</span>
+            </div>
+          </div>
+
+          <div className="col-span-1 row-start-4 flex w-full flex-col-reverse gap-3 lg:-mt-32 lg:flex-col">
+            <div className="group flex h-32 w-full cursor-pointer items-center justify-center bg-black-500/50 ease-out lg:h-56">
+              <p className="hidden p-8 text-2xl font-medium text-white-300 transition-all duration-500 group-hover:inline-flex">
+                ver projeto
+              </p>
+            </div>
+
+            <div>
+              <h2 className="text-xl font-bold uppercase tracking-widest">
+                Empresa x
+              </h2>
+
+              <p className="text-sm">
+                Criamos o novo website para esta empresa que vem crescendo muito
+                no mercado.
+              </p>
+
+              <span className="text-xs uppercase tracking-widest">Website</span>
+            </div>
+          </div>
+
+          <div className="col-span-1 row-start-4 flex w-full flex-col-reverse gap-3 lg:flex-col">
+            <div className="group flex h-32 w-full cursor-pointer items-center justify-center bg-black-500/50 ease-out lg:h-56">
+              <p className="hidden p-8 text-2xl font-medium text-white-300 transition-all duration-500 group-hover:inline-flex">
+                ver projeto
+              </p>
+            </div>
+
+            <div>
+              <h2 className="text-xl font-bold uppercase tracking-widest">
+                Empresa x
+              </h2>
+
+              <p className="text-sm">
+                Criamos o novo website para esta empresa que vem crescendo muito
+                no mercado.
+              </p>
+
+              <span className="text-xs uppercase tracking-widest">Website</span>
+            </div>
+          </div>
+
+          <div className="col-span-2 row-start-4 flex w-full flex-col-reverse gap-3 lg:-mt-32 lg:flex-col">
+            <div className="group flex h-32 w-full cursor-pointer items-center justify-center bg-black-500/50 ease-out lg:h-56">
+              <p className="hidden p-8 text-2xl font-medium text-white-300 transition-all duration-500 group-hover:inline-flex">
+                ver projeto
+              </p>
+            </div>
+
+            <div>
+              <h2 className="text-xl font-bold uppercase tracking-widest">
+                Empresa x
+              </h2>
+
+              <p className="text-sm">
+                Criamos o novo website para esta empresa que vem crescendo muito
+                no mercado.
+              </p>
+
+              <span className="text-xs uppercase tracking-widest">Website</span>
+            </div>
+          </div>
+
+          <div className="col-start-3 row-start-5 flex w-full flex-col-reverse gap-3 lg:mt-12 lg:flex-col">
+            <div className="group flex h-32 w-full cursor-pointer items-center justify-center bg-black-500/50 ease-out lg:h-56">
+              <p className="hidden p-8 text-2xl font-medium text-white-300 transition-all duration-500 group-hover:inline-flex">
+                ver projeto
+              </p>
+            </div>
+
+            <div>
+              <h2 className="text-xl font-bold uppercase tracking-widest">
+                Empresa x
+              </h2>
+
+              <p className="text-sm">
+                Criamos o novo website para esta empresa que vem crescendo muito
+                no mercado.
+              </p>
+
+              <span className="text-xs uppercase tracking-widest">Website</span>
+            </div>
+          </div>
+
+          <div className="lg:mt-24lg:flex-col col-start-1 col-end-2 row-start-5 flex w-full flex-col-reverse gap-3">
+            <div className="group flex h-32 w-full cursor-pointer items-center justify-center bg-black-500/50 ease-out lg:h-56">
+              <p className="hidden p-8 text-2xl font-medium text-white-300 transition-all duration-500 group-hover:inline-flex">
+                ver projeto
+              </p>
+            </div>
+
+            <div>
+              <h2 className="text-xl font-bold uppercase tracking-widest">
+                Empresa x
+              </h2>
+
+              <p className="text-sm">
+                Criamos o novo website para esta empresa que vem crescendo muito
+                no mercado.
+              </p>
+
+              <span className="text-xs uppercase tracking-widest">Website</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-28 flex justify-end">
+          <p className="text-sm font-semibold">Integre a galeria</p>
+        </div>
+        <div className="relative mt-2 h-px w-full bg-blue-500/80 before:absolute before:-top-1 before:right-0 before:h-1 before:w-8 before:bg-blue-500/80"></div>
+      </section>
+    </main>
+  );
+}
